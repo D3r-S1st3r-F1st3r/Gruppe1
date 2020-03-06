422c7d16b958820091b3277c05196b6937b2db8b
D3r-S1st3r-F1st3r <peter.martin.zanker@gmail.com>
2020-03-06 08:45:14 +0100

    test

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -4,4 +4,15 @@ import javax.swing.*;
 
 public class BoardView extends JFrame {
 
+    public BoardView(){
+
+    }
+
+    public void initStartScreen(){
+
+    }
+
+    public void initGame(){
+
+    }
 }
