84b0b27a5f8cf315f7a17e83ae12004f8c4d57c4
D3r-S1st3r-F1st3r <peter.martin.zanker@gmail.com>
2020-03-06 09:22:49 +0100

    erste Schritte

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -9,7 +9,6 @@ public class BoardView extends JFrame {
     JFrame jfr;             //Basisfenster
 
     JPanel startScreen;     //JPanel in dem der Startscreen angezeigt wird
-
     JButton startGame;      //Button, der das Spiel startet
     JButton endGame;        //Button, der das Spiel beendet
 
@@ -21,19 +20,40 @@ public class BoardView extends JFrame {
     public BoardView(){
         jfr = new JFrame();
         jfr.setTitle("Minesweeper");
-        jfr.setPreferredSize(new Dimension(board_width,board_height));
+        jfr.setSize(board_width,board_height);
         jfr.setResizable(false);
+        jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfr.setVisible(true);
+
+        //Fenster werden erstmal erstellt
         initStartScreen();
+        initGame();
+
     }
 
     public void initStartScreen(){
-        startGame = new JButton("Spiel starten");
+        startScreen = new JPanel();                 //Panel wird erstellt
+
+        startGame = new JButton("Spiel starten");      //Button wird erstellt und in das Panel eingefügt
+        startScreen.add(startGame);
     }
 
     public void initGame(){
-
+        gameScreen = new JPanel();
     }
+
+    //Startscreen wird im Fenster angezeigt
+    public void addStartScreen(){
+        jfr.add(startScreen);
+    }
+
+    //Spiel wird im Fenster angezeigt
+    public void addGameScreen(){
+        jfr.remove(startScreen);
+        jfr.add(gameScreen);
+    }
+
+    //Buttonreaktion
     public JButton getStartButton(){
         return this.startGame;
     }
