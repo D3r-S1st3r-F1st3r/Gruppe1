b91aee6acc5b135e2fedde66ba7bd8bb1f2a0b15
D3r-S1st3r-F1st3r <peter.martin.zanker@gmail.com>
2020-03-06 08:48:18 +0100

    test

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -4,6 +4,17 @@ import javax.swing.*;
 
 public class BoardView extends JFrame {
 
+    //Komponenten
+    JFrame jfr;             //Basisfenster
+
+    JPanel startScreen;     //JPanel in dem der Startscreen angezeigt wird
+
+    JButton startGame;      //Button, der das Spiel startet
+    JButton endGame;        //Button, der das Spiel beendet
+
+    JPanel gameScreen;      //JPanel in dem das Spielfeld angezeigt wird
+
+
     public BoardView(){
 
     }
